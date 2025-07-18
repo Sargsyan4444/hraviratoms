0f36882846c91fbaa3db5f0e2a80fa1a8739f21c
Sargsyan4444 <ruzan.sargsyan.444@gmail.com>
2025-07-18 04:37:03 -0700

    i finisheed

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.churchBtn').addEventListener('click', function() {
+    window.open('https://www.google.com/maps/place/Shoghakat+Church/@40.1677477,44.302928,665m/data=!3m1!1e3!4m6!3m5!1s0x406a9515ac1be109:0xc3954800b9f65e94!8m2!3d40.1681084!4d44.3047948!16s%2Fm%2F05zj49g?entry=ttu&g_ep=EgoyMDI1MDcxNS4xIKXMDSoASAFQAw%3D%3D', '_blank');
+});
+document.querySelector('.restoranBtn').addEventListener('click', function() {
+    window.open('https://www.google.com/maps/place/nor+surmalu/data=!4m2!3m1!1s0x406ac10063a7b1e1:0xb7bafbb6179d284e?sa=X&ved=1t:242&ictx=111', '_blank');
+});
